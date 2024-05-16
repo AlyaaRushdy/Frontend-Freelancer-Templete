d1f0a0f6e52ae290589af03092e7a0ddb2d78c85
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-05-16 07:18:56 +0300

    updating navbar based on scroll position JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 const dir = "./images/portfolio-images/";
+const headerHeight = document.getElementById("header").offsetHeight;
+const imageContainer = document.getElementById("images-container");
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav__a");
 let imgNamesList = [
   "cabin.png",
   "cake.png",
@@ -9,6 +13,7 @@ let imgNamesList = [
 ];
 let imageElements = "";
 
+// adding images to portfolio section
 imgNamesList.forEach((img) => {
   const imgElement = new Image();
   imgElement.setAttribute("src", dir + img);
@@ -16,5 +21,21 @@ imgNamesList.forEach((img) => {
   imgElement.setAttribute("class", "portfolio__img");
   imageElements += imgElement.outerHTML;
 });
+imageContainer.innerHTML = imageElements;
 
-document.getElementById("images-container").innerHTML = imageElements;
+// updating navbar based on current section
+window.addEventListener("scroll", () => {
+  let scrollPosition = window.scrollY + headerHeight;
+  sections.forEach((section) => {
+    if (scrollPosition >= section.offsetTop) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        if (
+          section.getAttribute("id") === link.getAttribute("href").substring(1)
+        ) {
+          link.classList.add("active");
+        }
+      });
+    }
+  });
+});
